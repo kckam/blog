921d67ddbbe5985a678951573a6cf2c4e3c9ae39
DESKTOP-4S2F2RT\Kok Chin <kokchin@diningbutler.com>
2018-09-12 11:06:25 +0800

    'here'

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -40,7 +40,7 @@
 	<body>
 		<div class="container">
 			<div class="content">
-				<div class="title">Laravel 5 YEAH</div>
+				<div class="title">Laravel 5 YEAH 2</div>
 				<div class="quote">{{ Inspiring::quote() }}</div>
 			</div>
 		</div>
